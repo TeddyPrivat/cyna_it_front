35234160e885e2f5fdcb8dc9235f9eb0ad031867
TeddyPrivat <47928474+TeddyPrivat@users.noreply.github.com>
2025-04-16 14:37:20 +0200

    Test liaison avec l'API, composant Test.vue, test réussi et fonctionnel

---
+++ src/components/Test.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>TEST API</h1>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+
+<script setup lang="ts">
+import {onMounted, ref} from "vue";
+import axios from "axios";
+
+const message = ref('Chargement...');
+
+onMounted(async () => {
+  try{
+    const res = await axios.get('http://localhost:8000/api/test');
+    message.value = res.data.message;
+  }catch(error){
+    console.log(error);
+    message.value = "Erreur de connexion à l'API."
+  }
+})
+
+</script>
+
+
+
+<style scoped>
+
+</style>
